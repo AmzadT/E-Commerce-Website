73c0e9e8ab103efca0868f12447021c01dbfe261
AmzadT <amzadhussain7250@gmail.com>
2024-11-25 16:34:12 +0530

    admin url changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -70,7 +70,7 @@ const Navbar = () => {
                                     <hr className='w-[100px] border-none h-[0.5px] bg-gray-300 ml-4' />
                                     <p onClick={logout} className='cursor-pointer hover:text-black text-center'>LogOut</p>
                                     <hr className='w-[100px] border-none h-[0.5px] bg-gray-300 ml-4' />
-                                    <p onClick={() => navigate('/https://shop-sphere-admin-theta.vercel.app')} className='cursor-pointer hover:text-black text-center'>Admin Panel</p>
+                                    <a href='https://shop-sphere-admin-theta.vercel.app' target='_blank' onClick={() => navigate('/admin')} className='cursor-pointer hover:text-black text-center'>Admin Panel</a>
                                 </div>
                             </div>
                         }
