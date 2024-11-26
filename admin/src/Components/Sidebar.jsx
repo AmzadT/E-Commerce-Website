4264a9fbb9f4da7c269ca24b603bdb453d55cafa
AmzadT <amzadhussain7250@gmail.com>
2024-11-27 01:21:59 +0530

    new styling changes

--- admin/src/Components/Sidebar.jsx
+++ admin/src/Components/Sidebar.jsx
@@ -7,19 +7,19 @@ const Sidebar = () => {
             <div className="flex flex-col gap-4 pt-6 pl-6 sm:pl-10 md:pl-[15%] text-sm md:text-[15px]">
 
                 {/* Add Items */}
-                <NavLink to='/add' className='flex items-center gap-3 border border-gray-300 px-3 py-2 rounded-sm hover:bg-green-300 mr-5'>
+                <NavLink to='/add' className='flex items-center gap-3 border border-gray-300 px-3 py-2 rounded-sm hover:bg-green-400 mr-5'>
                     <img className="w-5 h-5" src={assets.add_icon} alt="Add Icon" />
                     <p className="hidden sm:block">Add Items</p>
                 </NavLink>
 
                 {/* List Items */}
-                <NavLink to='/list' className='flex items-center gap-3 border border-gray-300 px-3 py-2 rounded-sm hover:bg-green-300 mr-5'>
+                <NavLink to='/list' className='flex items-center gap-3 border border-gray-300 px-3 py-2 rounded-sm hover:bg-green-400 mr-5'>
                     <img className="w-5 h-5" src={assets.order_icon} alt="List Icon" />
                     <p className="hidden sm:block">List Items</p>
                 </NavLink>
 
                 {/* Orders */}
-                <NavLink to='/orders' className='flex items-center gap-3 border border-gray-300 px-3 py-2 rounded-sm hover:bg-green-300 mr-5'>
+                <NavLink to='/orders' className='flex items-center gap-3 border border-gray-300 px-3 py-2 rounded-sm hover:bg-green-400 mr-5'>
                     <img className="w-5 h-5" src={assets.order_icon} alt="Orders Icon" />
                     <p className="hidden sm:block">Orders</p>
                 </NavLink>
